19381333173b56973298206c303932bc2e05ffa9
wcc <1296650532@qq.com>
2022-10-05 12:09:36 +0800

    hot-fix commit3

--- src/main/java/com/git.java
+++ src/main/java/com/git.java
@@ -5,5 +5,6 @@ public class git {
         System.out.println("hot-fix分支");
         System.out.println("冲突合并master");
         System.out.println("冲突合并hot-fix");
+        System.out.println("冲突合并hot-fix1");
     }
 }
